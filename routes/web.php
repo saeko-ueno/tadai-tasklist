6603f446a4ca089681b184f785807a68cc96675f
jiro.kirameki <saeko.ueno@techacademy.jp>
2018-06-08 06:54:08 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/', 'TasksController@index');
 Route::resource('tasks', 'TasksController');
+
+Route::get('signup','Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
