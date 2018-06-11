94564169f7413a6020f4c15fbb61382e50dd1bf9
jiro.kirameki <saeko.ueno@techacademy.jp>
2018-06-11 04:56:51 +0000

    user pages

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -18,8 +18,8 @@
             <ul class='nav navbar-nav navbar-right'>
                 
             @if (Auth::check())
-               <li>{!! link_to_route('tasks.create', '新規タスクの投稿')!!}<li>
-               <li class="dropdown">
+                <li>{!! link_to_route('users.index', 'Users') !!}</li>
+                <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                         <ul class="dropdown-menu">
                             <li><a href="#">My task</a></li>
